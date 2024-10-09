7d4d0df4b30d6737f30aea17a9fb436fa3a9f765
ndush <mdamarisnduku@gmail.com>
2024-10-09 13:53:21 +0300

    edit onboarding form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,9 @@
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,75 +12,115 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import CustomFormField from "../CustomFormField"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomFormField from "../CustomFormField";
 
 export enum FormFieldType {
-    INPUT = 'input',
-    TEXTAREA = 'textarea',
-    PHONE_INPUT= 'phoneInput',
-    CHECKBOX='checkbox',
-    DATE_PICKER='datePicker',
-    SELECT='select',
-    SKELETON='skeleton',
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
 }
- 
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-})
- 
-const PatientForm=() => {
+});
+
+const PatientForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
     },
-  })
- 
+  });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-      console.log(values)
+    console.log(values);
   }
   return (
-    <div><Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 flex-1"
+        >
+          <section className="mb-12 space-y-4">
             <h1>Hi there 👋</h1>
-            <p className="text-dark-700">Schedule your first appointment</p>
-        </section>
-        <CustomFormField
-          fieldType={FormFieldType.INPUT}
-          control={form.control}
-          name="name"
-          label="Full name"
-          placeholder="John Does"
-          iconSrc="/assets/icons/user.svg"
-          iconAlt="user"
-        />
+            <p className="text-dark-700">Get Started with Appointments.</p>
+          </section>
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="name"
+            label="Full name"
+            placeholder="Adrian Hajdin"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user"
+          />
 
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-    />
-     <Button type="submit">Submit</Button>
-    </form>
-  </Form></div>
-  )
-}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email address</FormLabel>
+                <FormControl>
+                  <div className="relative flex items-center">
+                    <img
+                      src="/assets/icons/email.svg"
+                      alt="Email Icon"
+                      className="absolute left-3"
+                    />
+                    <Input
+                      placeholder="adrian@jsmastery.pr"
+                      {...field}
+                      className="pl-10"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-export default PatientForm
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <div className="relative flex items-center">
+                    <img
+                      src="/assets/icons/lead.svg"
+                      alt="Phone Icon"
+                      className="absolute left-3"
+                    />
+                    <Input
+                      placeholder="+00 456-7890"
+                      {...field}
+                      className="pl-10"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="btn-get-started" type="submit">
+            Get Started
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default PatientForm;
